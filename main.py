b5c3ad2299709d2178cddee72d89d4c413acaf69
Championsh <77329576+Championsh@users.noreply.github.com>
2023-03-02 02:51:19 +0300

    GUI-Nodes. Max Sons for all nodes

---
+++ main.py
@@ -0,0 +1,37 @@
+import json
+import glob
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+
+def sonSearch(searchlist):
+    max_sons = 0
+    if "children" in searchlist:
+        max_sons = len(searchlist["children"])
+        for i in range(max_sons):
+            if type(searchlist["children"][i]) == type(list):
+                tmp_sons = sonSearch(searchlist["children"][i])
+            else:
+                break
+            if max_sons < tmp_sons:
+                max_sons = tmp_sons
+    return max_sons
+
+
+histlist = {}
+path = "D:\\combined/19*.json"
+for filename in tqdm(glob.glob(path)):
+    data = json.load(open(filename, 'r'))["activity"]["root"]
+    sons = sonSearch(data)
+    if sons in histlist:
+        histlist[sons] += 1
+    else:
+        histlist[sons] = 1
+    #print("MAX_SONS:", sons)
+
+#print(histlist)
+histtmp = [key for key, val in histlist.items() for _ in range(val)]
+plt.hist(histtmp, bins=40, color='blue', edgecolor='black')
+plt.title('MAX_GUI_SONS')
+plt.xlabel('Nodes')
+plt.ylabel('MAX_SONS')
+plt.show()
