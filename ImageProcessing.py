a9f8df7a956e85b9acd64a97e3f3d92c85556315
Championsh <77329576+Championsh@users.noreply.github.com>
2023-03-02 10:15:08 +0300

    Update ImageProcessing.py

--- ImageProcessing.py
+++ ImageProcessing.py
@@ -3,16 +3,19 @@ import glob
 from tqdm import tqdm
 from fractions import Fraction
 
-unique = []
-path = "D:\\combined/195*.jpg"
+unique = {}
+path = "D:\\combined/19*.jpg"
+res_path = "D:\\ImageProcessing/res.json"
 for filename in tqdm(glob.glob(path)):
     im = cv2.imread(filename)
-    h, w, c, = im.shape
+    h, w, c = im.shape
     tmp_s = str(h) + 'x' + str(w)
-    # print(tmp_s)
-    # print(Fraction(h, w))
-    unique.append(tmp_s)
-    unique.append(Fraction(h, w))
+    if tmp_s not in unique:
+        unique[tmp_s] = 1
+        tmp = Fraction(h, w)
+        tmp_s = str(tmp.numerator) + '/' + str(tmp.denominator)
+        if tmp_s not in unique:
+            unique[tmp_s] = 1
 
 unique = set(unique)
 print(unique)
