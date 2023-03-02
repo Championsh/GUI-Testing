c7bbfd3fbc7310a6634ac224c3e0fa0f9e42b4df
Championsh <77329576+Championsh@users.noreply.github.com>
2023-03-03 00:13:39 +0300

    Problems fixed + new processing method

--- ImageProcessing.py
+++ ImageProcessing.py
@@ -1,20 +1,22 @@
-import cv2
+from PIL import Image
 import glob
-from tqdm import tqdm
 from fractions import Fraction
+import argparse
 
-unique = []
-path = "D:\\combined/19*.jpg"
+parser = argparse.ArgumentParser()
 
-for filename in tqdm(glob.glob(path)):
-    im = cv2.imread(filename)
-    h, w, c = im.shape
-    tmp_s = str(h) + 'x' + str(w)
-    if tmp_s not in unique:
-        unique.append(tmp_s)
+parser.add_argument("-path", "--path", type=str,
+                    help="Input the data's path, if not specified=D:\\\\combined/*.jpg",
+                    default="D:\\\\combined/*.jpg")
+
+args = parser.parse_args()
+
+unique = set()
+for filename in glob.glob(args.path):
+    with Image.open(filename) as im:
+        h, w = im.size[0], im.size[1]
+        unique.add(f"{h}x{w}")
         tmp = Fraction(h, w)
-        tmp_s = str(tmp.numerator) + '/' + str(tmp.denominator)
-        if tmp_s not in unique:
-            unique.append(tmp_s)
+        unique.add(f"{tmp}")
 
 print(unique)
