0f53f42cefbb1d9dad3cdc2b52604359b1bcebef
Championsh <77329576+Championsh@users.noreply.github.com>
2023-03-02 10:30:48 +0300

    Update ImageProcessing.py
    Upd: Словарь -> Массив

--- ImageProcessing.py
+++ ImageProcessing.py
@@ -3,19 +3,18 @@ import glob
 from tqdm import tqdm
 from fractions import Fraction
 
-unique = {}
+unique = []
 path = "D:\\combined/19*.jpg"
-res_path = "D:\\ImageProcessing/res.json"
+
 for filename in tqdm(glob.glob(path)):
     im = cv2.imread(filename)
     h, w, c = im.shape
     tmp_s = str(h) + 'x' + str(w)
     if tmp_s not in unique:
-        unique[tmp_s] = 1
+        unique.append(tmp_s)
         tmp = Fraction(h, w)
         tmp_s = str(tmp.numerator) + '/' + str(tmp.denominator)
         if tmp_s not in unique:
-            unique[tmp_s] = 1
+            unique.append(tmp_s)
 
-unique = set(unique)
 print(unique)
