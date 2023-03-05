b39ae7822ebe7dbc80b0d743eae95f5dacad4d07
Championsh <77329576+Championsh@users.noreply.github.com>
2023-03-05 12:58:08 +0300

    Proper PostOrder Traversal Func
    Recursion was displaced with proper traversal.

--- GUI.py
+++ GUI.py
@@ -2,6 +2,7 @@ import argparse
 import json
 import glob
 import matplotlib.pyplot as plt
+from collections import deque
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-path", "--path", type=str,
@@ -27,37 +28,51 @@ def recursive_sonSearch(searchlist):
     return max_sons
 
 
-tmp_max = 0
-
-
 def postorder_traversal(searchlist):
-    global tmp_max
-    if isinstance(searchlist, dict) and OBJECT not in searchlist:
-        return
-    elif searchlist is None:
-        return
-    cur_list = searchlist[OBJECT]
-    sons = len(cur_list)
-    for i in range(sons):
-        postorder_traversal(cur_list[i])
-    if tmp_max < sons:
-        tmp_max = sons
+    max_sons = 0
+    if not searchlist:
+        return 0
+    child_mas = deque([searchlist])
+
+    while child_mas:
+        cur_son = child_mas.popleft()
+        if isinstance(cur_son, dict) and OBJECT in cur_son:
+            cur_list = cur_son[OBJECT]
+            tmp_queue = deque()
+            tmp_len = 0
+            for tmp_son in cur_list:
+                if tmp_son is not None:
+                    tmp_queue.append(tmp_son)
+                    tmp_len += 1
+            tmp_queue.append(tmp_len)
+            child_mas.extendleft(tmp_queue)
+        elif isinstance(cur_son, int):
+            if max_sons < cur_son:
+                max_sons = cur_son
+    return max_sons
 
 
 histlist = []
+cmp_output_fl = 1
 for filename in glob.glob(args.path):
     data = json.load(open(filename, 'r'))["activity"]["root"]
     if args.func == "po":
-        postorder_traversal(data)
-        histlist.append(tmp_max)
+        histlist.append(postorder_traversal(data))
         tmp_max = 0
     elif args.func == "r":
         sons = recursive_sonSearch(data)
         histlist.append(sons)
     else:
-        postorder_traversal(data)
-        print(f"{recursive_sonSearch(data)}vs{tmp_max}")
+        if cmp_output_fl:
+            print("CMP START, see DIFFs below:")
+            cmp_output_fl = 0
+        val1 = recursive_sonSearch(data)
+        val2 = postorder_traversal(data)
+        if val1 != val2:
+            print(f"{val1} vs {val2}")
         tmp_max = 0
+if not cmp_output_fl:
+    print("CMP END")
 
 plt.hist(histlist, bins=10, color='blue', edgecolor='black')
 plt.title('MAX_GUI_SONS')
